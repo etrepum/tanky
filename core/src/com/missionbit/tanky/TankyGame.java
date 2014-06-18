194b01071a56e1021655355cfffdb51cf7cc2099
Bob Ippolito <bob@redivi.com>
2014-06-18 12:10:33 -0700

    build the terrain

--- core/src/com/missionbit/tanky/TankyGame.java
+++ core/src/com/missionbit/tanky/TankyGame.java
@@ -2,26 +2,69 @@ package com.missionbit.tanky;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.util.Random;
 
 public class TankyGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+    static final float WIDTH = 800;
+    static final float HEIGHT = 480;
+    float[] terrain;
+    OrthographicCamera camera;
+    ShapeRenderer shapeRenderer;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        camera = new OrthographicCamera();
+        camera.setToOrtho(true, WIDTH, HEIGHT);
+        shapeRenderer = new ShapeRenderer();
+        newTerrain();
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            public boolean touchDown (int x, int y, int pointer, int button) {
+                // your touch down code here
+                return true; // return true to indicate the event was handled
+            }
+
+            public boolean touchUp (int x, int y, int pointer, int button) {
+                // your touch up code here
+                return true; // return true to indicate the event was handled
+            }
+        });
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        updateGame();
+        drawGame();
 	}
+
+    private void newTerrain() {
+        terrain = new TerrainBuilder(new Random(), WIDTH, HEIGHT, (float)0.6, (float)0.5).build();
+    }
+
+    public void updateGame () {
+        if (Gdx.input.isTouched()) {
+            newTerrain();
+        }
+    }
+
+    public void drawGame () {
+        Gdx.gl.glClearColor((float)135.0/255, (float)206.0/255, (float)235.0/255, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        camera.update();
+        shapeRenderer.setProjectionMatrix(camera.combined);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(1, 1, 0, 1);
+        int h = (int)HEIGHT;
+        for (int i = 0; i < (int)WIDTH - 1; i++) {
+            shapeRenderer.triangle(i, terrain[i], i + 1, h, i, h);
+            shapeRenderer.triangle(i, terrain[i], i + 1, terrain[i + 1], i + 1, h);
+        }
+        shapeRenderer.end();
+    }
 }
---
+++ core/src/com/missionbit/tanky/TerrainBuilder.java
@@ -0,0 +1,77 @@
+package com.missionbit.tanky;
+
+import java.util.ArrayDeque;
+import java.util.Random;
+
+/**
+ * http://www.somethinghitme.com/2013/11/11/simple-2d-terrain-with-midpoint-displacement/
+ */
+public class TerrainBuilder {
+    final float m_width;
+    final float m_height;
+    final float m_roughness;
+    final float m_displace;
+    final Random m_rng;
+    static final class TerrainStep {
+        final int m_lo;
+        final int m_hi;
+        final float m_displace;
+        TerrainStep(int lo, int hi, float displace) {
+            m_lo = lo;
+            m_hi = hi;
+            m_displace = displace;
+        }
+        int mid() {
+            return m_lo + (m_hi - m_lo) / 2;
+        }
+    }
+    TerrainBuilder(Random rng, float width, float height, float displace, float roughness) {
+        m_rng = rng;
+        m_width = width;
+        m_height = height;
+        m_displace = displace;
+        m_roughness = roughness;
+    }
+    private float calcPoint(float lo, float hi, float displace) {
+        return ((lo + hi) / 2) + (m_rng.nextFloat() * 2 * displace) - displace;
+    }
+
+    private void recursive_midpoint(float[] points, int lo, int hi, float a, float b, float displace) {
+        int mid = lo + (hi - lo) / 2;
+        if (mid > lo) {
+            displace = displace * m_roughness;
+            float c = calcPoint(a, b, displace);
+            points[mid] = c;
+            recursive_midpoint(points, lo, mid, a, c, displace);
+            recursive_midpoint(points, mid, hi, c, b, displace);
+        }
+    }
+    float[] build() {
+        // rounded up to an odd number
+        int size = (int)m_width | 1;
+        float[] points = new float[size];
+        float displace = m_displace * m_height;
+        // Set the initial left and right points
+        points[0] = calcPoint(0, m_height, displace * (float)0.5);
+        points[size - 1] = calcPoint(0, m_height, displace * (float)0.5);
+        ArrayDeque<TerrainStep> steps = new ArrayDeque<TerrainStep>(size / 2);
+        if (size > 2) {
+            steps.add(new TerrainStep(0, size - 1, displace));
+        }
+        while (!steps.isEmpty()) {
+            TerrainStep s = steps.removeFirst();
+            int mid = s.mid();
+            int lo = s.m_lo;
+            int hi = s.m_hi;
+            displace = s.m_displace * m_roughness;
+            points[mid] = calcPoint(points[lo], points[hi], displace);
+            if (mid - lo > 1) {
+                steps.add(new TerrainStep(lo, mid, displace));
+            }
+            if (hi - mid > 1) {
+                steps.add(new TerrainStep(mid, hi, displace));
+            }
+        }
+        return points;
+    }
+}
